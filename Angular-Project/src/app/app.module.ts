32bce001da11a206d659a1cd49e525e2ebb63ad9
akash <harivignesh0707@gmail.com>
2022-12-19 12:38:28 +0530

    no message

--- Angular-Project/src/app/app.module.ts
+++ Angular-Project/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { RouteConfigLoadEnd, RouterModule, ROUTES } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
+    RouterModule.forRoot([]),
     BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    FormsModule,
+    AppRoutingModule,
+    MatDialogModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
