d7c4e5cc3596caa8fea59d73bb505cb25f67e0bb
akash <harivignesh0707@gmail.com>
2022-12-19 12:53:47 +0530

    no message

--- Angular-Project/src/app/app-routing.module.ts
+++ Angular-Project/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {
     path:'',
-    component:HomeComponent,
+    component:HeaderComponent,
   }
 ];
 
