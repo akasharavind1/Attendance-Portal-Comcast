f7d0007b9e9b26dd363392df532685287b181c49
Hari <harivignesh0707@gmail.com>
2022-12-26 14:06:17 +0530

    no message

--- Angular-Project/src/app/admin/admin.component.ts
+++ Angular-Project/src/app/admin/admin.component.ts
@@ -23,7 +23,7 @@ export class AdminComponent {
   getEmployeeList(){
     this.httpClient.get("http://localhost:8080/api/v1/Employee").subscribe((result: any)=>{
       this.employeeList= result;
-
+      
     })
   }
 
