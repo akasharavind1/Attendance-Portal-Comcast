8700e378357ec14bb1e631b5cde0c358cde72664
akash <harivignesh0707@gmail.com>
2022-12-26 15:35:14 +0530

    ..

--- Angular-Project/src/app/home/home.component.ts
+++ Angular-Project/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -9,7 +12,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class HomeComponent {
 
 
-  constructor (private form:FormBuilder){}
+  constructor (private form:FormBuilder,
+    private httpClient: HttpClient,private router:Router){}
 
     //this is formbuilder method 
     validateemployee = this.form.group({
@@ -18,7 +22,16 @@ export class HomeComponent {
 
     })
 
-
+    getEmployeeList(){
+      this.httpClient.get("http://localhost:8080/api/v1/Employee").subscribe((result: any)=>{
+  
+        if(result.roles=='admin')
+        {
+          this.router.navigateByUrl('/admin')
+        }
+  
+      })
+    }
   }
 
   // this is form group method
