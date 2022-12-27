7e6e88ba5eb550e8302e48fe450a43ad8c7a7f3e
APAC\aa127 <Akash@A@comcast.com>
2022-12-27 13:33:22 +0530

    no message

--- Angular-Project/src/app/servicefiles/servicefiles.service.ts
+++ Angular-Project/src/app/servicefiles/servicefiles.service.ts
@@ -1,25 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
 @Injectable({
   providedIn: 'root'
-
 })
 export class ServicefilesService {
-
-  constructor(private http:HttpClient) { 
-    
-    }
-
-    getEmployeeList(){
-      return this.http.get("http://localhost:8080/api/v1/employee")
-        
-    }
-
-
-    postEmployee(body:any){
-      return this.http.post("http://localhost:8080/api/v1/employee",body)
-        
-    }    
+  http: any;
+  getEmployeeList(){
+    return this.http.get("http://localhost:8080/api/v1/login")
+  }
+  postEmployee(body:any){
+    return this.http.post("http://localhost:8080/api/v1/login",body)
+  }
 }
