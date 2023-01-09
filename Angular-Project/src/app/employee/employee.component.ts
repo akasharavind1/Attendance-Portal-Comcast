fec4048ad52500411d7db1cda7f55640868f369c
Hari <harivignesh0707@gmail.com>
2023-01-09 12:40:01 +0530

    no message

--- Angular-Project/src/app/employee/employee.component.ts
+++ Angular-Project/src/app/employee/employee.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { ServicefilesService } from '../servicefiles/servicefiles.service';
-<<<<<<< HEAD
 import { HttpClient } from '@angular/common/http';
 
 
-=======
-import { Router } from '@angular/router';
->>>>>>> ce69a89efc74c1d44c6f01a194fc39cfa9d6778c
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent {
-<<<<<<< HEAD
  
   employeeList:any;
   constructor(private serviceData:ServicefilesService, private httpClient:HttpClient){
@@ -28,13 +23,4 @@ export class EmployeeComponent {
     })
   }
 
-=======
-  constructor(private serviceData:ServicefilesService, private router: Router){
-  }
-  logout(){
-    localStorage.removeItem('tokenuser');
-    localStorage.clear();
-        this.router.navigate(['/home']);
-   };
->>>>>>> ce69a89efc74c1d44c6f01a194fc39cfa9d6778c
 }
